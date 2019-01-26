7dde0bf1903e5ed2b7800be21d96d902e4d95f42
napayne <napaynedev@gmail.com>
2019-01-25 22:32:39 -0500

    Modified layer with copy and paste and cut.  Enabled auto shift

--- keyboards/fourier/keymaps/napaynedev/keymap.c
+++ keyboards/fourier/keymaps/napaynedev/keymap.c
@@ -16,13 +16,16 @@ enum custom_keycodes {
 #define KC_FN2 MO(_FN2)
 #define KC_SPFN1 LT(_FN1, KC_SPACE)
 #define KC_BSFN2 LT(_FN2, KC_BSPC)
+#define KC_COPY LCTL(KC_C)
+#define KC_PST LCTL(KC_V)
+#define KC_CUT LCTL(KC_X)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_DEL,  KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCOLON,          KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,             KC_M,    KC_COMM, KC_DOT,    KC_RSFT, KC_FN2,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_FN1,  KC_FN1,               KC_SPACE,                  KC_FN1,  KC_RALT,   KC_RGUI,  KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT, KC_FN1,  KC_FN1,                 KC_SPACE,                  KC_FN1,  KC_RALT,   KC_RGUI,  KC_RCTL
   ),
 
   [_FN1] = LAYOUT(
